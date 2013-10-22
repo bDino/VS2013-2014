ae97b6041da531599bb574e0696215ba895de74e
bDino <b.dino@arcor.de>
2013-10-22 14:21:20 +0200

    ClientKill und ServerKill eingebaut

--- A1/2 - Server/server.erl
+++ A1/2 - Server/server.erl
@@ -23,7 +23,8 @@ start() ->
     %Serverkomponenten initialisieren
     QueuemanagerPID = spawn(fun() -> queuemanager:start(DlqLimit) end),
     ClientmanagerPID = spawn(fun() -> clientmanager:start(Clientlifetime,QueuemanagerPID) end),
-    ServerPID = spawn(fun() -> loop(ClientmanagerPID,QueuemanagerPID,0) end),
+    
+    ServerPID = spawn(fun() -> loop(self(),Lifetime,ClientmanagerPID,QueuemanagerPID,0) end),
     
     global:register_name(Servername,ServerPID),
     logging("server.log","...Server started and registered with Servername ...\n"),
@@ -34,15 +35,20 @@ start() ->
 
 %% -------------------------------------------
 %% loop-Funktion behandelt eingehende Nachrichten und wird immer wieder aufgerufen
-loop(CManager,QManager,MessageNumber) ->
+loop(ServerPID,Lifetime,CManager,QManager,MessageNumber) ->
+
+TRef = timer:send_after(Lifetime * 1000,ServerPID,{exit}),
+    
     receive
-        {getmsgid, ClientPID} -> 
+        {getmsgid, ClientPID} ->
+            timer:cancel(TRef), 
             logging("server.log",lists:concat(["Server: Received getmsgid von Client: ",pid_to_list(ClientPID)," ! ",MessageNumber,"\n"])),
             ClientPID ! {nnr,MessageNumber},
             NewMsgNr = MessageNumber+1,
-            loop(CManager, QManager, NewMsgNr);
+            loop(ServerPID,Lifetime,CManager, QManager, NewMsgNr);
 
         {getmessages,ClientPID} ->
+            timer:cancel(TRef),
             logging("server.log",lists:concat(["Server: Received getmessages von Client: ",pid_to_list(ClientPID),"\n"])),
             CManager ! {getmessages, ClientPID, self()},
             receive
@@ -50,16 +56,19 @@ loop(CManager,QManager,MessageNumber) ->
                     ClientPID ! {reply,MsgId,Message,Terminated}
             end,
             
-            loop(CManager, QManager, MessageNumber);
+            loop(ServerPID,Lifetime,CManager, QManager, MessageNumber);
         
         {dropmessage, {Nachricht, Nr}} -> 
+            timer:cancel(TRef),
             logging("server.log",lists:concat(["Server: Received dropmessage: ",Nachricht,"\n"])),
             QManager ! {dropmessage, {Nachricht, Nr}},
         
-            loop(CManager, QManager, MessageNumber);
+            loop(ServerPID,Lifetime,CManager, QManager, MessageNumber);
         
-        exit -> 
-            logging("server.log",lists:concat(["Server Received Exit Signal and is Shuting down\n"])),
+        {exit} -> 
+            logging("server.log","Server Received Exit Signal and is Shuting down\n"),
             exit("Serverlifetime is over")
     end
 .
+
+
