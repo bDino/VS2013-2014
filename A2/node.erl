024ef639ede431e3bc1f8d5c12975cfbcbce4379
Milena Dreier <mille_3@hotmail.de>
2013-10-26 16:02:30 +0200

    scheiße runtergecodet! nicht compiliert! fehlt ganz viel bla!

--- A2/node.erl
+++ A2/node.erl
@@ -37,41 +37,153 @@ loop(NodeName, NodeLevel, NodeState, EdgeList, ThisFragName) ->
     
         receive
             {connect, Level, Edge} ->
-                loop(NodeName, Level, State);
+                {Weight, Neighbour, _self} = Edge,
+                case NodeLevel>Level of
+                    true -> 
+                        NewEdgeList = changeEdgeState(EdgeList, Edge, branch),
+                        Neighbour ! {initiate, NodeLevel, ThisFragName, NodeState, {Weight, NodeName, Neighbour};
+                    false ->
+                        case (getEdgeState(EdgeList, Edge)==basic) of
+                            true ->
+                                %%gar nichts
+                            false ->
+                                Neighbour ! {initiate, NodeLevel+1, Weight, find, {Weight, NodeName, Neighbour}
+                                %%nicht sicher über die nächsten Schritte
+                                %NewNodeLevel = NodeLevel+1,
+                                %NewFragName = Weight;
+                        end
+                    end
+                
+            
+                loop(NodeName, NodeLevel, NodeState, EdgeListe, ThisFragName);
+            
             
             {initiate, Level, FragName, State, Edge} ->
-                {Weight, Neighbour, EdgeState} = edges:searcgAKmG(EdgeList),
-                Neighbour ! {Test, NodeLevel, ThisFragName, {Weight, NodeName, Neighbour}},
+                NewNodeLevel = Level,
+                NewFragName = FragName,
+                NewNodeState = State,
+                {Weight, Neighbour, _self} = Edge,
+                InBranch = Neighbour,
+                %%einkommende Kante speichern (Edge)
+                sendInitiate(EdgeList, Edge),
+                %%wenn State=find dann find-count raufsetzen
+                
+                case State == find of
+                    true ->
+                        test_procedure(EdgeList)
+                end,
         
-                receive
-                    {accept, Edge} ->
-                        %changeEdgeState for Edge to Branch!
-                    {reject, Edge} ->
-                        %changeEdgeState for Edge to Rejected!
-                end;
+                loop(NodeName, NewNodeLevel, NewNodeState, EdgeList, NewFragName);
         
-                loop(NodeName, Level, State);
-        
+            
+           
             {test, Level, FragName, Edge} ->
+                %%aufwecken wenn er schläft
+                %%wenn Level größer als NodeLevel dann warten...siehe unten
                 {Weight, Neighbour, _self} = Edge,
                 ThisEdge = {Weight, NodeName, Neighbour},
                 case FragName == ThisFragName of
                     true ->  
                         %changeEdgeState for Edge: ThisEdge to Rejected!
-                        Neighbour ! {reject, ThisEdge};
-                    false ->    
+                        case (getEdgeState(EdgeList, Edge) == basic) of
+                            true -> NewEdgeList = changeEdgeState(EdgeList, Edge, rejected),
+                                    case TestNode == Neighbour of
+                                        true -> test_procedure(EdgeList);
+                                        false -> Neighbour ! {reject, ThisEdge}
+                                    end;
+                            false -> NewEdgeList = EdgeList
+                        end;
+                        
+                    false ->  
+                        NewEdgeList = EdgeList,
                         case NodeLevel >= Level of
                             true ->     Neighbour ! {accept, ThisEdge};
                             false ->    %%Warten bis NodeLevel sich verändert hat
                         end
                 end,
-                loop(NodeName
+                loop(NodeName, NodeLevel, NodeState, NewEdgeList, ThisFragName);
                 
         
+            %%---------------------------------------------------------------------------
+            %% TestNode muss mit übergeben werden und BestWeight und BestNode vorher gespeichert werden!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            {accept, Edge} ->
+                NewTestNode = nil,
+                {Weight, Neighbour, _self} = Edge,
+                case (Weight < BestWeight) of
+                    true -> 
+                        NewBestNode = Neighbour,
+                        NewBestWeight = Weight;
+                       
+                    false ->
+                        NewBestNode = BestNode,
+                        NewBestWeight = BestWeight
+                end,
+                case (FindCount == 0) of       
+                    true -> 
+                        NewNodeState = found,
+                        InBranch ! {report, NewBestWeight};
+                    false ->
+                        NewNodeState = NodeState
+                    end,
+            
+                loop(NodeName, NodeLevel, NewNodeState, EdgeList, ThisFragName);
+            
+                        
+                    
             {report, Weight, Edge} ->
             %% getBranches and send report over Branch-Edges
             %% if Branch == Core 
-                loop(NodeName, Level, State);
+                {Weight, Neighbour, _self} = Edge,
+                case InBranch = Edge of
+                    true ->
+                        case NodeState == find of
+                            true ->
+                                NewEdgeList = EdgeList,
+                                %%warten;
+                            false ->
+                                case Weight>BestWeight of
+                                    true -> 
+                                        %%changeroot procedure
+                                        case (getEdgeState(BestNode) == branch) of
+                                            {BNWeight, BestNodeNeighbour, _self} = BestNode,
+                                            true ->
+                                                NewEdgeList = EdgeList,
+                                                BestNodeNeighbour ! {changeroot, {BNWeight, NodeName, BestNodeNeighbour}};
+                                            false ->
+                                                BestNodeNeighbour ! {connect, NodeLevel, {BNWeight, NodeName, BestNodeNeighbour}},
+                                                NewEdgeList = changeEdgeState(EdgeList, BestNode, branch)
+                                        end;
+                                    false -> 
+                                        NewEdgeList = EdgeList,
+                                        case (Weight == BestWeight == ) of
+                                            true ->
+                                                exit
+                                                %%Algo ist fertig!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!            EXIT EXIT EXIT EXIT
+                                        end
+                                end 
+                        end;
+            
+                    false ->
+                        NewEdgeList = EdgeList,
+                        NewFindCount = FindCound - 1,
+                        case Weight<BestWeight of
+                            true ->
+                                NewBestWeight = Weight,
+                                NewBestNode = Neighbour;
+                            false ->
+                                NewBestWeight = BestWeight,
+                                NewBestNode = BestNode
+                        end,
+                        case FindCount == 0 andalso TestNode == nil of
+                            true ->
+                                NewNodeState = found,
+                                {IBWeight, InNeighbour, _self} = InBranch,
+                                InNeighbour ! {report, NewBestWeight, {IBWeight, NodeName, InNeighbour}};
+                            false ->
+                                NewNodeState = NodeState
+                        end
+                end,
+                loop(NodeName, NodeLevel, NewNodeState, NewEdgeList, ThisFragName);
         
             {changeroot, Edge} ->
             %% sende Changeroot weiter nach außen(an alle außer an die von denen es herkommt???)
@@ -79,3 +191,13 @@ loop(NodeName, NodeLevel, NodeState, EdgeList, ThisFragName) ->
         end
     
 .
+
+
+searchAKmG(EdgeList) ->
+
+    %findNeSL muss umgebaut werden und die Kante mit dem kleinsten Gewicht und dem Status basic ausgeben
+    Edge = findNeSL(EdgeList),
+    Edge
+
+.
+    
