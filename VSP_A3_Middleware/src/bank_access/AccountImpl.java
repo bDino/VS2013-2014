2fc9ea9e383a8fc6c5f8efda9210352e839dbf52
Milena Dreier <mille_3@hotmail.de>
2013-12-05 10:34:22 +0100

    CommunicationModule use

--- VSP_A3_Middleware/src/bank_access/AccountImpl.java
+++ VSP_A3_Middleware/src/bank_access/AccountImpl.java
@@ -2,18 +2,18 @@ package bank_access;
 
 import java.net.Socket;
 
-import mware_lib.CommunicationHelper;
+import mware_lib.CommunicationModule;
 
 public class AccountImpl extends AccountImplBase {
 
 	String host;
 	String port;
-	CommunicationHelper cHelper;
+	CommunicationModule cHelper;
 	
 	public AccountImpl(String host, String port){
 		this.host = host;
 		this.port = port;
-		cHelper = new CommunicationHelper(new Socket(host, port));
+		cHelper = new CommunicationModule(new Socket(host, port));
 	}
 	
 	@Override
