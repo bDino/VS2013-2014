c895751a3eaf16c6aeb2599f661005d7423d0cd9
Milena Dreier <mille_3@hotmail.de>
2013-12-05 11:57:33 +0100

    manager und transaction

--- VSP_A3_Middleware/src/bank_access/ManagerImpl.java
+++ VSP_A3_Middleware/src/bank_access/ManagerImpl.java
@@ -1,10 +1,27 @@
 package bank_access;
 
+
+
+import mware_lib.CommunicationModule;
+
 public class ManagerImpl extends ManagerImplBase{
 
+	//TODO: name?!
+	String host;
+	int port;
+	CommunicationModule commModule;
+	
+	public ManagerImpl(String host, int port){
+		this.host = host;
+		this.port = port;
+		commModule = new CommunicationModule(host, port);
+	}
 	@Override
 	public String createAccount(String owner, String branch) {
-		// TODO Auto-generated method stub
+		Object[] args = new Object[]{owner, branch};
+		Class[] classes = new Class[]{String.class, String.class};
+		commModule.invokeRemoteMethod("ManagerImplBase|Manager|createAccount|"+args+"|"+classes);
+		
 		return null;
 	}
 
--- VSP_A3_Middleware/src/cash_access/TransactionImpl.java
+++ VSP_A3_Middleware/src/cash_access/TransactionImpl.java
@@ -1,25 +1,52 @@
 package cash_access;
 
+import java.util.UUID;
+
+import mware_lib.CommunicationModule;
+import mware_lib.Reply;
+
 public class TransactionImpl extends TransactionImplBase {
 
+	//TODO: name?!
+	String host;
+	int port;
+	CommunicationModule commModule;
+	
+	public TransactionImpl(String host, int port){
+		this.host = host;
+		this.port = port;
+		commModule = new CommunicationModule(host, port);
+	}
+	
 	@Override
 	public void deposit(String accountId, double amount)
 			throws InvalidParamException {
-		// TODO Auto-generated method stub
+		
+		Object[] args = new Object[]{accountId, amount};
+		Class[] classes = new Class[]{String.class, double.class};
+		Reply reply = commModule.invokeRemoteMethod("ManagerImplBase|createAccount|"+args+"|"+classes);
 
 	}
 
 	@Override
 	public void withdraw(String accountId, double amount)
 			throws InvalidParamException, OverdraftException {
-		// TODO Auto-generated method stub
-
+		
+		Object[] args = new Object[]{accountId, amount};
+		Class[] classes = new Class[]{String.class, double.class};
+		Reply reply = commModule.invokeRemoteMethod("ManagerImplBase|createAccount|"+args+"|"+classes);
 	}
 
 	@Override
-	public double getBalance(String accountId) throws InvalidParamException {
-		// TODO Auto-generated method stub
-		return 0;
+	public double getBalance(String accountId) 
+			throws InvalidParamException {
+		Object[] args = new Object[]{accountId};
+		Class[] classes = new Class[]{String.class};
+		Reply reply = commModule.invokeRemoteMethod("ManagerImplBase|createAccount|"+args+"|"+classes);
+		
+		if(reply. return 0;
 	}
+	
+	
 
 }
