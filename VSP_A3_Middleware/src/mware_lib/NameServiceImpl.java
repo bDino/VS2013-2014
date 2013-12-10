4b06b9d072d0a0fd2cc7f0e55862262b1db58ebe
bDino <buskulic.dino@gmail.com>
2013-12-10 16:53:54 +0100

    Minimal changes

--- VSP_A3_Middleware/src/mware_lib/NameServiceImpl.java
+++ VSP_A3_Middleware/src/mware_lib/NameServiceImpl.java
@@ -8,8 +8,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 // CommunikationFormat: [ClassType|ObjectName|ObjectMethod|MethodParamObjectArray|ParamClassArray|SUCCESS/ERROR]
-public class NameServiceImpl extends NameServiceImplBase 
-{
+public class NameServiceImpl extends NameServiceImplBase {
 
 	String gnsServiceName;
 	int gnsPort;
@@ -21,17 +20,18 @@ public class NameServiceImpl extends NameServiceImplBase
 	BufferedReader answerReader;
 	LocalObjectPool objectPool;
 
-	public NameServiceImpl(String serviceName, int port, LocalObjectPool objectPool, int serverListenerPort, String serverListenerHost) 
-	{
+	public NameServiceImpl(String serviceName, int port,
+			LocalObjectPool objectPool, int serverListenerPort,
+			String serverListenerHost) {
 		this.gnsServiceName = serviceName;
 		this.gnsPort = port;
 		this.objectPool = objectPool;
 		this.serverListenerPort = serverListenerPort;
 		this.serverListenerHost = serverListenerHost;
 	}
-	
-	private void initializeConnection() throws UnknownHostException,IOException 
-	{
+
+	private void initializeConnection() throws UnknownHostException,
+			IOException {
 		socket = new Socket(this.gnsServiceName, this.gnsPort);
 		in = new InputStreamReader(socket.getInputStream());
 		out = socket.getOutputStream();
@@ -39,17 +39,17 @@ public class NameServiceImpl extends NameServiceImplBase
 	}
 
 	@Override
-	public void rebind(Object servant, String name) 
-	{
-		System.out.println("NameServie rebind called: " + servant.getClass().getSimpleName() + " - "
-				+ name);
+	public void rebind(Object servant, String name) {
+		System.out.println("NameServie rebind called: "
+				+ servant.getClass().getSimpleName() + " - " + name);
 		objectPool.rebindLocalSkeleton(name, servant);
 
 		if (this.socket == null || this.socket.isClosed()) {
 			try {
 				initializeConnection();
-				out.write(("rebind" + "#" + servant.getClass().getSimpleName() + "#" + name + "#" 
-						+ serverListenerHost + "#" + serverListenerPort + "\n").getBytes());
+				out.write(("rebind" + "#" + servant.getClass().getSimpleName()
+						+ "#" + name + "#" + serverListenerHost + "#"
+						+ serverListenerPort + "\n").getBytes());
 				System.out.println(answerReader.readLine());
 				closeAllConnections();
 			} catch (IOException e) {
@@ -61,39 +61,37 @@ public class NameServiceImpl extends NameServiceImplBase
 	}
 
 	@Override
-	public Object resolve(String name)
-	{
+	public Object resolve(String name) {
 		System.out.println("NameService resolve called: - " + name);
 		String[] answer = null;
 
 		if (this.socket == null || this.socket.isClosed()) {
 			try {
 				initializeConnection();
+				closeAllConnections();
+
+				out.write(("resolve#" + name + "\n").getBytes());
+				answer = answerReader.readLine().replace(",", "").split("#");
+
+				closeAllConnections();
 			} catch (IOException e) {
 				e.printStackTrace();
 				closeAllConnections();
 			}
-		}
 
-		try {
-			out.write(("resolve#"+name + "\n").getBytes());
-			answer = answerReader.readLine().replace(",","").split("#");
-			closeAllConnections();
-		} catch (IOException e) {
-			e.printStackTrace();
-			closeAllConnections();
 		}
-		return new Stub(name,answer[1],Integer.parseInt(answer[2]));
+		
+		return new Stub(name, answer[1], Integer.parseInt(answer[2]));		
 	}
 
-	public void closeAllConnections() 
-	{
+	public void closeAllConnections() {
 		try {
 			socket.close();
 			in.close();
 			out.close();
 		} catch (IOException e) {
-			System.out.println("Error closing Connections: \n" + e.getMessage());
+			System.out
+					.println("Error closing Connections: \n" + e.getMessage());
 		}
 
 	}
